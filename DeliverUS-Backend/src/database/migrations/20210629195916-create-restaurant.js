9fa4d0cc709230b7a8f33b475748039bb9366391
David <davchacum@alum.us.es>
2024-02-23 10:13:52 +0100

    prueba

--- DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
+++ DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
@@ -6,7 +6,76 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
+      },
+      restaurantCategoryId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'RestaurantCategories'
+          },
+          key: 'id'
+        },
+        
+        
+        name: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        description: {
+          allowNull: true,
+          type: Sequelize.STRING,
+        },
+        address: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        postalCode: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        url: {
+          allowNull: true,
+          type: Sequelize.STRING,
+        },
+        shippingCosts: {
+          allowNull: false,
+          type: Sequelize.DOUBLE,
+        },
+        averageServiceMinutes: {
+          allowNull: true,
+          type: Sequelize.DOUBLE,
+        },
+        email: {
+          allowNull: true,
+          type: Sequelize.STRING,
+        },
+        phone: {
+          allowNull: true,
+          type: Sequelize.STRING,
+        },
+        logo: {
+          allowNull: true,
+          type: Sequelize.STRING,
+        },
+        heroImage: {
+          allowNull: true,
+          type: Sequelize.STRING,
+        },
+        status: {
+          allowNull: true,
+          type: Sequelize.ENUM,
+          values: [
+            'online',
+            'offline',
+            'closed',
+            'temporily closed'
+
+          ],
+        },
+        
+        
       }
+      
       // TODO: Include the rest of the fields of the Restaurants table
 
     })
